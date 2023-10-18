e5aac98e74ab445893c283753e8f3ae9cf2c1415
Shavkat Aynurin <shavkat.aynurin@gmail.com>
2023-10-18 07:15:43 -0700

    Cleanup

--- archive/MazeGridOptions.cs
+++
@@ -1,7 +0,0 @@
-
-public class MazeGridOptions {
-    public const int RoomsCountAuto = -1;
-    public const int RoomsCountNone = -2;
-
-    public int RoomsCount { get; set; } = RoomsCountAuto;
-}
